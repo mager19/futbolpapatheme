2fee51c2b46b4867754f9ddd47fbdbc63c7f7d62
Mario Germán Reyes <mager19@gmail.com>
2023-04-23 15:39:00 -0500

    conditional checkout script

--- inc/scripts.php
+++ inc/scripts.php
@@ -5,6 +5,8 @@ function baseTheme_scripts()
     wp_enqueue_style('manrope', 'https://fonts.googleapis.com/css2?family=Manrope:wght@400;800&display=swap');
     wp_enqueue_style('google-font-icon', 'https://fonts.googleapis.com/icon?family=Material+Icons');
     wp_enqueue_style('main', get_template_directory_uri() . '/assets/dist/css/app.css', array(), '1.1.4', 'all');
-    wp_enqueue_script('theme-scripts', get_template_directory_uri() . '/assets/dist/js/app.js', array('jquery'), '0.1', true);
+    if (!is_checkout()) {
+        wp_enqueue_script('theme-scripts', get_template_directory_uri() . '/assets/dist/js/app.js', array('jquery'), '0.1', true);
+    }
 }
 add_action('wp_enqueue_scripts', 'baseTheme_scripts');
