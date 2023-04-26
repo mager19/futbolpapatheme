b7cd3ed4675f5fa61055eb35f576815544120be3
Mario Germán Reyes <mager19@gmail.com>
2023-04-25 19:25:03 -0500

    fix search template design

--- search.php
+++ search.php
@@ -16,35 +16,43 @@ get_header(); ?>
         <?php
         if (have_posts()) : ?>
 
-            <header class="page-header">
-                <h1 class="title--3 title-md--1">
+            <header class="page-header px-4">
+                <h1 class="inline-flex border-b-2 border-secondary title--4 lg:title--4 mb-8">
                     <?php
                     /* translators: %s: search query. */
                     printf(esc_html__('Search Results for: %s', 'frontporchsolutions'), '<span>' . get_search_query() . '</span>');
                     ?>
                 </h1>
             </header><!-- .page-header -->
-            <div class="grid grid-cols-3">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-4">
                 <?php
                 /* Start the Loop */
                 while (have_posts()) : the_post(); ?>
                     <!-- Item Post -->
+                    <!-- Item Post -->
                     <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                        <div class="header-post">
-                            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-                        </div>
+                        <header class="header-post">
+                            <a href="<?php the_permalink(); ?>">
+                                <?php the_post_thumbnail(); ?>
+                            </a>
+                        </header>
 
                         <div class="info-post">
-                            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                            <h2 class='title title--5 lg:title--5'>
+                                <a class='text-text visited:text-text hover:text-text  hover:opacity-80 no-underline mt-4 inline-flex' href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                            </h2>
 
-                            <?php the_excerpt(); ?>
+                            <?php echo wp_trim_words(get_the_content(), 30, '[...]'); ?>
 
-                            <a href="<?php the_permalink(); ?>" class="link">
-                                Continue Reading
-                                <i class="fa fa-angle-right"></i>
+                            <?php //the_excerpt(); 
+                            ?>
+
+                            <a class='flex mt-2' href="<?php the_permalink(); ?>" class="link">
+                                <?php _e('Continue Reading', 'frontporchsolutions'); ?>
                             </a>
                         </div>
                     </article>
+                    <!-- Item Post -->
                     <!-- Item Post -->
                 <?php endwhile; ?>
             </div>
