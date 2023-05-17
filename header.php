b1b36f79b8acdbe1b462e3bcec0259484a526604
Mario Germán Reyes <mager19@gmail.com>
2023-05-17 07:25:49 -0500

    remove test

--- header.php
+++ header.php
@@ -32,18 +32,18 @@
     
     
     <?php 
-                        $current_user = wp_get_current_user();
-                        $user_membership = wc_memberships_get_user_membership( $current_user->ID );
+                        // $current_user = wp_get_current_user();
+                        // $user_membership = wc_memberships_get_user_membership( $current_user->ID );
 
 
 
-                        if ( $user_membership ) {
-                            $plan_id = $user_membership->plan_id;
-                            $plan_name = get_post_field( 'post_title', $plan_id );
-                            echo 'El usuario ' . $current_user->user_login . ' está suscrito al plan ' . $plan_name;
-                        } else {
-                            echo 'El usuario ' . $current_user->user_login . ' no está suscrito a ningún plan de membresía.';
-                        }
+                        // if ( $user_membership ) {
+                        //     $plan_id = $user_membership->plan_id;
+                        //     $plan_name = get_post_field( 'post_title', $plan_id );
+                        //     echo 'El usuario ' . $current_user->user_login . ' está suscrito al plan ' . $plan_name;
+                        // } else {
+                        //     echo 'El usuario ' . $current_user->user_login . ' no está suscrito a ningún plan de membresía.';
+                        // }
                         
                     ?>
 
